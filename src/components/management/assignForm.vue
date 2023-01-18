04ae6886eba39089661dc82eff5460082efa959a
KissesJun <gwills@163.com>
2023-01-18 22:10:41 +0800

    teacher center.vue 不知道怎么设计下拉框的展示和提交

--- src/components/management/assignForm.vue
+++ src/components/management/assignForm.vue
@@ -1,19 +1,10 @@
 <template>
-  <div class="align-content-end">
-
-    <button :class="{'btn-success': isEditing, 'btn-primary': !isEditing}" class="btn"
-            type="button"
-            @click="handleEdit()">
-      <!--    if isEditing is true show save, if not, show edit-->
-      {{ isEditing ? '保存' : '编辑' }}
-
-    </button>
-  </div>
+  <div class="mb-3"></div>
   <form>
     <div class="row flex-fill">
       <div class="col-4">
         <div class="mb-3"><label class="form-label" for="username"><strong>作业名</strong><br></label><input
-            id="username" :disabled="!isEditing" :value="assign.name" class="form-control"
+            id="username" v-model="assign.name" :disabled="!isEditing" class="form-control"
             name="assignName"
             placeholder="请输入 作业名称"
             type="text"></div>
@@ -21,14 +12,14 @@
       <div class="col-4">
         <div class="mb-3 mb-md-0">
           <label class="form-label" for="first_name"><strong>截止日期</strong><br></label></div>
-        <input :disabled="!isEditing" :value="assign.ddl" class="form-control"
+        <input :disabled="!isEditing" v-model="assign.ddl" class="form-control"
                type="datetime-local">
       </div>
       <div class="col-4">
         <div class="mb-3">
           <label class="form-label"
                  for="first_name"><strong>格式化文件名</strong></label><input
-            id="first_name-2" :disabled="!isEditing" :value="assign.formatStr" class="form-control"
+            id="first_name-2" :disabled="!isEditing" v-model="assign.formatStr" class="form-control"
             name="formatFileName"
             placeholder="请输入 格式化文件名"
             type="text"></div>
@@ -41,13 +32,12 @@
           <strong>下发对象</strong>(Ctrl多选)<br></label>
         </div>
         <select id="dates-field2" :disabled="!isEditing" class="form-select multiselect-ui form-control"
+                v-model="forClazz"
                 multiple
         >
-          <!--                      use iteration to generate many options-->
-          <option v-for="clazz in assign.forClazz"
-                  :selected="clazz.isSelected"
-                  :value="clazz.id">{{ clazz.name }}
-          </option>
+          <!--                      use v-model forClazz to generate many options-->
+          <option v-for="clazz in assign.forClazz" :value="clazz.name" :selected="clazz.isSelected">{{clazz.name}}</option>
+<!--          <option v-for="clazz in assign.forClazz" :selected="clazz.isSelected">{{clazz.name}}</option>-->
 
         </select>
       </div>
@@ -55,6 +45,7 @@
         <div class="mb-3"><label class="form-label" for="email"><strong>描述</strong></label></div>
         <textarea :disabled="!isEditing" class="form-control"
                   name="assignDesc"
+                  v-model="assign.desc"
                   placeholder="请输入注明以下内容，文件格式，文件内容规则" rows="4">{{ assign.desc }}
                       </textarea>
       </div>
@@ -66,6 +57,7 @@
           <input
               :id="switchBtn.id"
               :checked="switchBtn.value"
+              v-model="switchBtn.value"
               :disabled="!isEditing"
               class="form-check-input pt-xl-0 mt-xl-1 me-xl-3"
               style="width: 49px;height: 24px;" type="checkbox">
@@ -75,33 +67,149 @@
         </div>
       </div>
     </div>
+    <div class="row flex-fill mb-auto">
+      <div class="col-12 d-flex justify-content-center">
+        <div class="mb-3"></div>
+        <div class="btn-group" role="group">
+          <!--    if isEditing is true show save, if not, show edit-->
+          <button :class="{'btn-success': isEditing, 'btn-primary': !isEditing}" class="btn"
+                  type="button"
+                  @click="isAdd ? handleCreate(assign, forClazz) : handleEdit(assign)">
+            <a v-if="isAdd">
+              {{ isEditing ? '提交' : '' }}
+            </a>
+            <a v-else-if="!isAdd">
+              {{ isEditing ? '保存' : '编辑' }}
+            </a>
+          </button>
+          <button class="btn btn-outline-info" type="button"
+                  :disabled="isEditing || isAdd"
+                  @click="handleReset(assign)">重置
+          </button>
+          <button  class="btn btn-danger"
+                  type="button"
+                   :disabled="isEditing || isAdd"
+                  @click="handleDelete()">删除
+          </button>
+        </div>
+      </div>
+    </div>
 
   </form>
 </template>
 
 <script>
-import {ref} from "vue";
 
 export default {
   name: "assignForm",
-  isEditing: ref(false),
-  // write a function to handle the click event
-  // when the button is clicked, change the value of isEditing
-  // if isEditing is true, show save, if not, show edit
   methods: {
-    handleEdit() {
+    handleCreate(assign, forClazz) {
+      // this.isEditing = !this.isEditing;
+      console.log("正在模拟提交");
+      console.log(assign);
+      console.log(forClazz);
+      this.forClazz = [];
+      this.handleReset(assign);
+    },
+    handleEdit(assign) {
       this.isEditing = !this.isEditing;
       console.log(this.isEditing);
+      console.log(assign)
+      // assign = {
+      //       isEditing: this.isEditing,
+      //       name: "111",
+      //       id: "new",
+      //       clazz: "1909班,1908班",
+      //       desc: " ",
+      //       formatStr: "111",
+      //       ddl: "",
+      //       forClazz: [
+      //         {
+      //           name: "1909班",
+      //           isSelected: false
+      //         },
+      //         {
+      //           name: "1908班",
+      //           isSelected: false
+      //         },
+      //         {
+      //           name: "1907班",
+      //           isSelected: false
+      //         }
+      //       ],
+      //       switches: [
+      //         {
+      //           id: "isPermitAny",
+      //           name: "允许匿名提交",
+      //           value: true
+      //         },
+      //         {
+      //           id: "isPermitLate",
+      //           name: "允许迟交",
+      //           value: true
+      //         },
+      //         {
+      //           id: "isVarifyName",
+      //           name: "校验名称",
+      //           value: true
+      //         }
+      //       ],
+      //     };
     },
+    handleReset(assign) {
+
+      // clear all value to default
+      assign.name = "已重置";
+      assign.forClazz =[
+          {
+            name: "1909班",
+            isSelected: true
+          },
+          {
+            name: "1908班",
+            isSelected: true
+          },
+          {
+            name: "1907班",
+            isSelected: false
+          }
+        ];
+      assign.desc = "";
+      assign.formatStr = "";
+      assign.ddl = "";
+      console.log("handleReseed");
+    },
+    handleDelete() {
+      console.log("删除");
+    },
+    // create an auto function that used by life cycle
+    beforeCreate: function () {
+      console.log("beforeCreate");
+    },
+
+
+    created() {
+      // this.assign = this.handleReset(this.assign);
+      console.log("created");
+    }
+
   },
-  // return function to use
+
   data() {
+    const isEditing = false | this.isAdd;
+    const forClazz = [];
     return {
-      isEditing: false,
+      // handleEdit: this.handleEdit,
+      isEditing,
+      forClazz,
     }
   },
 
   props: {
+    isAdd: {
+      type: Boolean,
+      default: false
+    },
     assign: {
 
       // name: '第一次作业',
@@ -113,6 +221,7 @@ export default {
       // isPermitAny: true,
       // isPermitLate: true,
       // isVarifyName: true,
+
     },
 
   },
