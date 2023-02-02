5227d406c5230a7f66a6e3a82e0d66293e12f738
KissesJun <gwills@163.com>
2023-02-02 16:28:46 +0800

    navBarSide.vue 接口写完

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,11 +1,4 @@
 <script setup>
-import Header from "../components/public/PublicHeader.vue";
-import Tab1Content from "../components/public/Tab1Content.vue";
-import Tab2Content from "../components/public/Tab2Content.vue";
-import Tab3Content from "../components/public/Tab3Content.vue";
-import Tab4Content from "../components/public/Tab4Content.vue";
-import Tab5Content from "../components/public/Tab5Content.vue";
-import PublicFooter from "@/components/public/PublicFooter.vue";
 </script>
 <template>
 <!-- generate the code that can jump to index or register ... etc page-->
@@ -16,8 +9,24 @@ import PublicFooter from "@/components/public/PublicFooter.vue";
 </template>
 
 <script>
+import Header from "../components/public/PublicHeader.vue";
+import Tab1Content from "../components/public/Tab1Content.vue";
+import Tab2Content from "../components/public/Tab2Content.vue";
+import Tab3Content from "../components/public/Tab3Content.vue";
+import Tab4Content from "../components/public/Tab4Content.vue";
+import Tab5Content from "../components/public/Tab5Content.vue";
+import PublicFooter from "@/components/public/PublicFooter.vue";
 import {defineComponent, getCurrentInstance, onMounted, reactive, ref} from "vue";
 export default defineComponent({
+  components:{
+    Header,
+    Tab1Content,
+    Tab2Content,
+    Tab3Content,
+    Tab4Content,
+    Tab5Content,
+    PublicFooter
+  },
   setup() {
     return {
       tableData: [
