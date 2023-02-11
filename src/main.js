65786f5bdab45019ae9e5b820ddb598e53b11fa3
KissesJun <gwills@163.com>
2023-02-12 01:37:01 +0800

    The update of main.js codes about Bootstrap lib is that the redundant import part has been decreasing. However, there are some parts still be remained,  So that you can know it apparently which lib in the project are you using.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
-// import '@babel/polyfill'
-// import 'mutationobserver-shim'
-// import './plugins/bootstrap-vue'
-// import bootstrapVue from "bootstrap-vue-3";
-import $ from 'jquery'
-import jquery from 'jquery';
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/router'
 import './api/mock.js'
 
-// import 'bootstrap'
-import './assets/bootstrap/css/bootstrap.min.css'
-import './assets/bootstrap/js/bootstrap.min.js'
 
-// Vue.prototype.jquery = $
 
 // import './assets/main.css'
 // FrontEnd - Part
@@ -23,34 +13,41 @@ import './assets/bootstrap/js/bootstrap.min.js'
 // import './assets/js/script.min.js' //  放在这里页面会白屏，只能放在index.html最下面
 
 // BackEnd - Part
-// import './backendAssets/js/theme.js'
-// import './backendAssets/bootstrap/css/bootstrap.min.css'
-// import './backendAssets/bootstrap/js/bootstrap.min.js'  // 打开后menu折叠框，无法关闭
+import './backendAssets/js/theme.js'
+import './backendAssets/bootstrap/css/bootstrap.min.css'
+import './backendAssets/bootstrap/js/bootstrap.min.js'  // 打开后menu折叠框，无法关闭
 
 
 const app = createApp(App)
 
 
 import api from './api/api.js'
+import $ from 'jquery'
+import jquery from 'jquery';
 app.config.globalProperties.$api = api
-// use BoostrapVue3
-// import BootstrapVue3 from 'bootstrap-vue-3'
-// // set the theme color of bootstrap
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-// app.use(BootstrapVue3)
+app.config.globalProperties.$ = $;
+app.config.globalProperties.jquery = jquery;
 
-// use BoostrapVue3
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import $ from 'jquery'
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-// import 'bootstrap/dist/js/bootstrap.min.js'
-// import 'font-awesome/css/font-awesome.min.css'
-// import { BootstrapVue,IconsPlugin} from 'bootstrap-vue'
-// app.use(BootstrapVue)
-// app.use(IconsPlugin)
-import "bootstrap/dist/css/bootstrap.min.css"
+
+
+import router from './router/router'
+import BootstrapVue3 from 'bootstrap-vue-3'
 app.use(router)
+app.use(BootstrapVue3)
 app.mount('#app')
 
+
+
+// use BoostrapVue3
+// 本地导入部分，不需要
+// import 'bootstrap'
+// import './assets/bootstrap/css/bootstrap.min.css'
+// import './assets/bootstrap/js/bootstrap.min.js'
+// 使用 npm install bootstrap，可使用
+import 'font-awesome/css/font-awesome.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.js"
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// npm install bootstrap-vue-3
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
