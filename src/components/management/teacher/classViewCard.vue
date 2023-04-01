d238925ae64f4cf3417074024200553f5e1668a1
KissesJun <gwills@163.com>
2023-04-01 18:31:06 +0800

    teacher classView 更改

--- src/components/management/teacher/classViewCard.vue
+++ src/components/management/teacher/classViewCard.vue
@@ -1,43 +1,76 @@
 <template>
-  <div class="col">
-    <div>
-      <div class="card">
-        <div class="d-flex pt-xl-3 pb-xl-2 mb-xl-4"
-             style="border-radius: 5px;border-width: 3px;border-color: var(--bs-blue);
-                   font-weight: bold;margin-top: 0;padding-top: 14px;">
-          <div class="flex-fill">
-            <h4>{{ clazz.name }}</h4>
-            <h6 class="text-muted mb-2">{{ clazz.teacher }}</h6>
-          </div>
-          <hr>
-          <div style="width: 84px;">
-            <button class="btn btn-primary d-flex ms-auto" type="button" @click="viewDetail(this.clazz.id)">详情</button>
-          </div>
-        </div>
-        <div class="table-responsive" style="border-color: rgb(100,129,217);">
-          <table class="table table-striped">
-            <tbody>
-            <tr v-if="assigns.length" v-for="assign in assigns">
-              <td>{{ assign.name }}</td>
-              <td colspan="3">
-                <div class="progress">
-                  <div :aria-valuenow="assign.progress" :class="assign.progress === 100 ? 'bg-success': 'bg-primary'"
-                       :style="'width: '+ assign.progress +'%;'"
-                       aria-valuemax="100"
-                       aria-valuemin="0"
-                       class="progress-bar">{{ assign.progress }}%
-                  </div>
-                </div>
-              </td>
-            </tr>
-            <tr v-else>暂无作业</tr>
-            </tbody>
-          </table>
-        </div>
-<!--        <div class="card-body ps-lg-2 pe-lg-2 pt-lg- pb-lg-3"></div>-->
-      </div>
-    </div>
-  </div>
+  <b-col>
+    <b-card
+         :header="assigns.length ? '进行中: '+assigns.length : '暂无数据'"
+         header-tag="header"
+         :header-class=" assigns.length ? 'text-white bg-primary' : 'text-white bg-secondary'"
+        img-top
+        no-body
+        style="max-width: 20rem;"
+    >
+      <b-card-body>
+        <b-card-title ><a href="http://www.baidu.com">{{ clazz.name }}</a></b-card-title>
+        <b-card-subtitle class="mb-2">{{ clazz.teacher }}</b-card-subtitle>
+        <b-card-text> {{ clazz.description }} </b-card-text>
+      </b-card-body>
+      <b-list-group flush v-if="assigns">
+        <b-list-group-item v-for="assign in assigns" >
+          <b-row>
+            <b-col cols="6">
+              {{ assign.name }}
+            </b-col>
+            <b-col cols="6">
+              <b-progress :value="assign.progress" :variant="assign.progress === 100 ? 'success': 'primary'"></b-progress>
+            </b-col>
+          </b-row>
+        </b-list-group-item>
+      </b-list-group>
+      <b-list-group flush v-else>
+        <b-list-group-item>暂无数据</b-list-group-item>
+      </b-list-group>
+      <b-card-footer :class=" assigns.length ? 'text-white bg-primary' : 'text-white bg-secondary'">共 {{ assigns.length }} 个作业</b-card-footer>
+    </b-card>
+  </b-col>
+  <!--  <div class="col">-->
+  <!--    <div>-->
+  <!--      <div class="card">-->
+  <!--        <div class="d-flex pt-xl-3 pb-xl-2 mb-xl-4"-->
+  <!--             style="border-radius: 5px;border-width: 3px;border-color: var(&#45;&#45;bs-blue);-->
+  <!--                   font-weight: bold;margin-top: 0;padding-top: 14px;">-->
+  <!--          <div class="flex-fill">-->
+  <!--            <h4>{{ clazz.name }}</h4>-->
+  <!--            <h6 class="text-muted mb-2">{{ clazz.teacher }}</h6>-->
+  <!--          </div>-->
+  <!--          <hr>-->
+  <!--          <div style="width: 84px;">-->
+  <!--            <button class="btn btn-primary d-flex ms-auto" type="button" @click="viewDetail(this.clazz.id)">详情</button>-->
+  <!--          </div>-->
+  <!--        </div>-->
+  <!--        <div class="table-responsive" style="border-color: rgb(100,129,217);">-->
+  <!--          <table class="table table-striped">-->
+  <!--            <tbody>-->
+
+  <!--            <tr v-if="assigns.length" v-for="assign in assigns">-->
+  <!--              <td>{{ assign.name }}</td>-->
+  <!--              <td colspan="3">-->
+  <!--                <div class="progress">-->
+  <!--                  <div :aria-valuenow="assign.progress" :class="assign.progress === 100 ? 'bg-success': 'bg-primary'"-->
+  <!--                       :style="'width: '+ assign.progress +'%;'"-->
+  <!--                       aria-valuemax="100"-->
+  <!--                       aria-valuemin="0"-->
+  <!--                       class="progress-bar">{{ assign.progress }}%-->
+  <!--                  </div>-->
+  <!--                </div>-->
+  <!--              </td>-->
+  <!--            </tr>-->
+  <!--            <tr v-else>暂无作业</tr>-->
+  <!--            </tbody>-->
+  <!--          </table>-->
+  <!--        </div>-->
+  <!--&lt;!&ndash;        <div class="card-body ps-lg-2 pe-lg-2 pt-lg- pb-lg-3"></div>&ndash;&gt;-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--  </div>-->
 </template>
 
 <script>
@@ -58,13 +91,13 @@ export default {
   methods: {
     viewDetail(clazzId) {
       // TODO: 实现查看详情的窗口跳转
-      console.log("查看班级作业收集详情" +clazzId)
+      console.log("查看班级作业收集详情" + clazzId)
     }
   },
   data() {
     const assigns = ref([]);
     const {proxy} = getCurrentInstance();
-    const getAssignByClass= async ()=> {
+    const getAssignByClass = async () => {
       assigns.value = await proxy.$api.getAssignsByClass(this.clazz.id);
       console.log(assigns.value)
     }
--- src/views/manage/teacher/ClassView.vue
+++ src/views/manage/teacher/ClassView.vue
@@ -1,37 +1,23 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12 col-sm-6 col-md-6">
+  <b-container fluid>
+    <b-row>
+      <b-col cols="12" sm="6" md="6" >
         <h3 class="text-dark mb-4">班级视图</h3>
-      </div>
-      <div class="col-12 col-sm-6 col-md-6 text-end" style="margin-bottom: 30px;">
-        <a class="btn btn-primary" role="button" @click="addNewClass">
-          <i class="fa fa-plus"></i>&nbsp;
+      </b-col>
+      <b-col cols="12" sm="6" md="6" class="text-end" style="margin-bottom: 30px;">
+        <b-button variant="primary" @click="addNewClass">
+          <i class="fa fa-plus"></i>
           新增班级
-        </a>
-      </div>
-    </div>
-<!--    <div id="TableSorterCard" class="card" style="margin-bottom: 19px;">-->
-<!--      <div class="card-header py-3">-->
-<!--        <div class="row table-topper align-items-center">-->
-<!--          <div class="col-12 col-sm-5 col-md-6 text-start" style="margin: 0px;padding: 5px 15px;">-->
-<!--            <p class="text-primary m-0 fw-bold">作业列表</p>-->
-<!--          </div>-->
-<!--          <div class="col-12 col-sm-7 col-md-6 text-end" style="margin: 0px;padding: 5px 15px;">-->
-<!--            <button class="btn btn-primary btn-sm reset" style="margin: 2px;" type="button">Borrar Filtros</button>-->
-<!--            <button id="zoom_in" class="btn btn-warning btn-sm" style="margin: 2px;" type="button"-->
-<!--                    zoomclick="ChangeZoomLevel(-10);"><i class="fa fa-search-plus"></i></button>-->
-<!--            <button id="zoom_out" class="btn btn-warning btn-sm" style="margin: 2px;" type="button"-->
-<!--                    zoomclick="ChangeZoomLevel(-10);"><i class="fa fa-search-minus"></i></button>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-      <div class="row gy-4 row-cols-1 row-cols-sm-2 row-cols-lg-3 text-center ms-xl-0 me-xl-0 mb-md-5 mt-xl-0 pt-xl-0"
-           style="padding-bottom: 0;margin-top: -25px;padding-top: 0;">
+        </b-button>
+      </b-col>
+    </b-row>
+    <b-row class="row gy-4 row-cols-1 row-cols-sm-2 row-cols-lg-3 text-center ms-xl-0 me-xl-0 mb-md-5 mt-xl-0 pt-xl-0"
+           style="padding-bottom: 0;margin-top: -25px;padding-top: 0;"
+    >
         <class-view-card :clazz="clazz" v-for="clazz in classes"/>
-      </div>
-  </div>
+    </b-row>
+
+  </b-container>
 </template>
 
 <script>
